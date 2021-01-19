3be06c8a51a72816368ea4d8db753aed4aac1719
wiisportsresort <wiisportsresort@users.noreply.github.com>
2021-01-19 16:10:48 +0000

    deploy: b6a0b308499b492e2f2d4178f50f3dce57784e50

--- _dist_/pages/Page2.js
+++
@@ -1,4 +0,0 @@
-import{a as n,b as _,e as u,f as p,g as c,h as v,n as l,r as d,u as m,v as i}from"/svelte-spa-template/chunk.FMBDKBHV.js";function f(r){let e,s;return{c(){e=v("div"),e.innerHTML=`<h2>very good page 2</h2> 
-
-  <span class="svelte-1hrk3va"><img src="svelte-horizontal.png" alt="very good svelte logo" width="160" class="svelte-1hrk3va"/>
-    is the best!!!!!</span>`},m(t,a){p(t,e,a)},p:n,i(t){s||l(()=>{s=d(e,_,{duration:100}),s.start()})},o:n,d(t){t&&c(e)}}}var o=class extends i{constructor(e){super();m(this,e,null,f,u,{})}},h=o;export{h as default};
